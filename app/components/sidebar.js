5215e2dd2ab83157d477a0483d589c1b8c2a1b37
Jeremie Ges <ges.jeremie@gmail.com>
2021-03-13 23:29:37 -0800

    feat: display user name in sidebar

--- app/components/sidebar.js
+++ app/components/sidebar.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 
 export default class SidebarComponent extends Component {
   @service session
+  @service currentUser
 
   @action
   signout() {
--- app/services/current-user.js
+++ app/services/current-user.js
@@ -3,9 +3,12 @@ import { inject as service } from '@ember/service';
 
 export default class CurrentUserService extends Service {
   @service session;
-  @service store;
 
   load() {
+    if (!this.session.isAuthenticated) {
+      return;
+    }
+
     let { __typename, ...user } = this.session.data.authenticated.signin;
 
     this.set('user', user);
